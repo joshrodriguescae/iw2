0550871ea1ebbf425325303eccd88eb6451f89b3
joshrodriguescae <125597302+joshrodriguescae@users.noreply.github.com>
2024-02-19 16:54:57 -0300

    Add files via upload

---
+++ Aula01/code.js
@@ -0,0 +1,17 @@
+
+function cnome(){
+    let n = document.getElementById("nome").value 
+    alert("Bem Vindo ao JS: " + n)
+}
+
+
+
+
+
+function ola(){
+    console.log("Olá JS2")
+    alert("Meu Front-end")
+}
+
+ola()
+
