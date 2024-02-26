7a529d9389f303f0e27b6b0b0bc05499bffc7810
joshrodriguescae <125597302+joshrodriguescae@users.noreply.github.com>
2024-02-26 16:57:22 -0300

    Add files via upload

---
+++ aula02/code.js
@@ -0,0 +1,41 @@
+//console.log("Olá JS")
+
+
+let c1 = "vermelho"
+let c2 = "verde"
+let c3 = "azul"
+let c4 = "amarelo"
+let c5 = "roxo"
+
+
+//Array - vetor
+const dados = [c1,c2,c3,c4,c5]
+
+   let cor = "verde"
+
+
+for(let i=0;i<dados.length;i++){
+
+    if (cor == dados[i]) {
+        console.log("Encontrado! " + i)
+    } else{
+        console.log("Não encontrado")
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
