366a3ca6f604ba165e06aa69c5838e091396a16b
joshrodriguescae <125597302+joshrodriguescae@users.noreply.github.com>
2024-04-01 16:51:29 -0300

    Add files via upload

---
+++ AulasJSON/app.js
@@ -0,0 +1,47 @@
+ function dados(){
+     const ds = [
+                 {id:1, login:"Ringo",senha:"1234", email:"ringo@gmail.com"},
+                 {id:2, login:"Paul",senha:"2222", email:"paul@gmail.com"},
+                 {id:3, login:"Lennon",senha:"3344", email:"lennon@gmail.com"}
+
+                ]
+     return ds 
+
+ }
+  function login (user,password){
+     
+     const usuarios = dados ()
+
+
+    for (let 1=0;i<usuarios.length;i++)
+     if(user == usuarios [1].login && password == usuario[1].senha ){
+            console.log("Você logou")
+     } 
+
+     
+
+  }
+  
+
+  function logar (){
+     let lg = document gett
+
+
+
+  }
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
