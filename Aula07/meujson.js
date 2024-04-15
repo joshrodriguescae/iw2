8de1df42a5f883922e528c4d3c020e317b3f9779
joshrodriguescae <125597302+joshrodriguescae@users.noreply.github.com>
2024-04-15 16:52:05 -0300

    Add files via upload

---
+++ Aula07/meujson.js
@@ -0,0 +1,67 @@
+function banco(){
+const dados=[
+    {
+        id:1,
+        login:"Ringo",
+        Senha:"1234",
+        email:"ringo@gmail.com"
+      
+
+     },
+       
+
+        
+    {
+      id:2,
+      login:"Paul",
+      Senha:"2222",
+      email:"paul@gmail.com", 
+
+    },
+     
+
+    {
+      id:3,
+      login:"Johe",
+      Senha:"3333",
+      email:"johe@gmail.com"
+
+    }
+              
+
+ ]
+  const ds =          
+             
+
+         
+              
+
+    }
+           
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
